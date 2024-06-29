540277826d25ec417d4a9bb22015bb47afa2a166
Abhishek N N <abhi8f@gmail.com>
2024-06-29 10:42:18 +0530

    added constants

---
+++ fa/src/app/core/constant/Constant.ts
@@ -0,0 +1,10 @@
+export const Constant = {
+    API_END_POINT: {
+        GET_DEPARTMENT: 'GetDepartments',
+        CREATE_DEPARTMENT: 'CreateDepartment'
+    },
+
+    VALIDATION_MESSAGE: {
+        REQUIRED: 'This Is Required'
+    }
+}
