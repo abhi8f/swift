428f593819fab341095129e84fe1334075366e62
Abhishek N N <abhi8f@gmail.com>
2024-06-29 12:17:21 +0530

    added routes table

--- fa/src/app/app.routes.ts
+++ fa/src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { LayoutComponent } from './page/layout/layout.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { EmployeeComponent } from './page/employee/employee.component';
+import { DepartmentComponent } from './page/department/department.component';
+import { TicketComponent } from './page/ticket/ticket.component';
+import { NewTicketComponent } from './page/new-ticket/new-ticket.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'employee',
+                component: EmployeeComponent
+            },
+            {
+                path: 'department',
+                component: DepartmentComponent
+            },
+            {
+                path: 'ticket',
+                component: TicketComponent
+            },
+            {
+                path: 'new-ticket',
+                component: NewTicketComponent
+            }
+        ]
+    }
+];
